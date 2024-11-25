00ff67ba14446e0dc37b67bd05879a531db74039
pablo dev <thisispablovester@gmail.com>
2024-11-25 11:44:40 -0300

    restore previous version main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-window.global ||= window;
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import theme from "./theme/index";
 import "./scss/styles.scss";
 import { ChakraProvider } from "@chakra-ui/react";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Index from "./components/Pages/WhoWeAre/(index)/Index";
 import Home from "./components/Pages/Home/Home";
@@ -18,24 +20,61 @@ import Toolbox from "./components/Pages/Toolbox/Toolbox";
 import ProjectPage from "./components/Pages/Portfolio/(index)/Index";
 import Error404Portfolio from "./components/Pages/Error404Portfolio/Error404Portfolio";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/home" replace />,
+    // errorElement: <ErrorPage/>
+  },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/who",
+        element: <WhoWeAre />,
+      },
+      {
+        path: "/who/:id",
+        element: <Index/>,
+        //errorElement: <Error404/>
+      },
+      {
+        path: "/what",
+        element: <WhatWeDo/>,
+      },
+      {
+        path: "/what",
+        element: <WhatWeDo/>,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />
+      },
+      {
+        path: "/portfolio/:id",
+        element: <ProjectPage />,
+        errorElement: <Error404Portfolio/>
+      },
+      {
+        path: "/toolbox",
+        element: <Toolbox/>,
+      },
+      {
+        path: "*",
+        element: <Error404/>,
+      }
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider theme={theme}>
-    <BrowserRouter>  {/* Use BrowserRouter directly */}
-      <Routes> {/* Use Routes component */}
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<Layout><Home /></Layout>} /> {/* Wrap route elements in Layout */}
-        <Route path="/who" element={<Layout><WhoWeAre /></Layout>} />
-        <Route path="/who/:id" element={<Layout><Index /></Layout>} />
-        <Route path="/what" element={<Layout><WhatWeDo /></Layout>} />
-        <Route path="/portfolio" element={<Layout><Portfolio /></Layout>} />
-        <Route
-          path="/portfolio/:id"
-          element={<Layout><ProjectPage /></Layout>}
-          errorElement={<Error404Portfolio />}
-        />
-        <Route path="/toolbox" element={<Layout><Toolbox /></Layout>} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </BrowserRouter>
-  </ChakraProvider>
+  // <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  // </React.StrictMode>
 );
